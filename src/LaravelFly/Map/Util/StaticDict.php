9afd768323f354e9feb0de370b71fcc525cb6bca
scil <scil.zc@gmail.com>
2018-07-26 16:07:11 +0800

    remove hardcode 1 in StaticDict

--- src/LaravelFly/Map/Util/StaticDict.php
+++ src/LaravelFly/Map/Util/StaticDict.php
@@ -20,35 +20,38 @@ trait StaticDict
 
     static public function initStaticForCorontine($cid, $listen = true)
     {
-        if ($cid > 0) {
+        if ($cid > WORKER_COROUTINE_ID) {
             static::$corStaticDict[$cid] = static::$corStaticDict[WORKER_COROUTINE_ID];
-        } else {
-            if (static::$arrayStaticAttri ?? false) {
-                foreach (static::$arrayStaticAttri as $attri) {
-                    static::$corStaticDict[WORKER_COROUTINE_ID][$attri] = [];
-                }
-            }
-            if (static::$normalStaticAttri ?? false) {
-                foreach (static::$normalStaticAttri as $attri => $defaultValue) {
-                    if (is_callable($defaultValue)) {
-                        static::$corStaticDict[WORKER_COROUTINE_ID][$attri] = $defaultValue();
-                    } else {
-                        static::$corStaticDict[WORKER_COROUTINE_ID][$attri] = $defaultValue;
-                    }
-                }
-            }
-
-            if ($listen) {
-                Container::getInstance()->make('events')->listen('request.corinit', function ($cid) {
-                    static::initStaticForCorontine($cid);
-                });
-
-                Container::getInstance()->make('events')->listen('request.corunset', function ($cid) {
-                    static::delStaticForCoroutine($cid);
-                });
-            }
-
+            return;
         }
+
+
+        if (static::$arrayStaticAttri ?? false) {
+            foreach (static::$arrayStaticAttri as $attri) {
+                static::$corStaticDict[WORKER_COROUTINE_ID][$attri] = [];
+            }
+        }
+        if (static::$normalStaticAttri ?? false) {
+            foreach (static::$normalStaticAttri as $attri => $defaultValue) {
+                if (is_callable($defaultValue)) {
+                    static::$corStaticDict[WORKER_COROUTINE_ID][$attri] = $defaultValue();
+                } else {
+                    static::$corStaticDict[WORKER_COROUTINE_ID][$attri] = $defaultValue;
+                }
+            }
+        }
+
+        if ($listen) {
+            Container::getInstance()->make('events')->listen('request.corinit', function ($cid) {
+                static::initStaticForCorontine($cid);
+            });
+
+            Container::getInstance()->make('events')->listen('request.corunset', function ($cid) {
+                static::delStaticForCoroutine($cid);
+            });
+        }
+
+
     }
 
     static function delStaticForCoroutine(int $cid)
