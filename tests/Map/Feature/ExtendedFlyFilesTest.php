06493b47cec5b528d3bac0a246f471aeabaa9810
scil <scil.zc@gmail.com>
2018-08-01 22:10:06 +0800

    add test partfile

--- tests/Map/Feature/ExtendedFlyFilesTest.php
+++ tests/Map/Feature/ExtendedFlyFilesTest.php
@@ -7,6 +7,12 @@ use LaravelFly\Tests\Map\MapTestCase;
 class ExtendedFlyFilesTest extends MapTestCase
 {
 
+    var $partFileMap=[
+        'extended/partfile/CookieJar.php' => '/vendor/laravel/framework/src/Illuminate/Cookie/CookieJar.php',
+        'extended/partfile/CookieJarSame.php' => '/vendor/laravel/framework/src/Illuminate/Cookie/CookieJar.php',
+
+        'extended/partfile/CookieSessionHandler.php' => '/vendor/laravel/framework/src/Illuminate/Session/CookieSessionHandler.php',
+    ];
 
     var $map = [
 
@@ -19,17 +25,13 @@ class ExtendedFlyFilesTest extends MapTestCase
         'extended/AuthManager.php' => '/vendor/laravel/framework/src/Illuminate/Auth/AuthManager.php',
 
 
-        'extended/CookieJar.php' => '/vendor/laravel/framework/src/Illuminate/Cookie/CookieJar.php',
         'extended/CookieServiceProvider.php' => '/vendor/laravel/framework/src/Illuminate/Cookie/CookieServiceProvider.php',
 
-        'extended/Connectors/ConnectionFactory.php' => '/vendor/laravel/framework/src/Illuminate/Database/Connectors/ConnectionFactory.php',
-        'extended/Connectors/MySqlConnector.php' => '/vendor/laravel/framework/src/Illuminate/Database/Connectors/MySqlConnector.php',
-        'extended/DatabaseManager.php' => '/vendor/laravel/framework/src/Illuminate/Database/DatabaseManager.php',
-        'extended/DatabaseServiceProvider.php' => '/vendor/laravel/framework/src/Illuminate/Database/DatabaseServiceProvider.php',
-        'extended/MySqlConnection.php' => '/vendor/laravel/framework/src/Illuminate/Database/MySqlConnection.php',
+        'extended/ConnectionFactory.php' => '/vendor/hhxsv5/laravel-s/src/Illuminate/Database/ConnectionFactory.php',
+        'extended/DatabaseManager.php' => '/vendor/hhxsv5/laravel-s/src/Illuminate/Database/DatabaseManager.php',
+        'extended/DatabaseServiceProvider.php' => '/vendor/hhxsv5/laravel-s/src/Illuminate/Database/DatabaseServiceProvider.php',
 
 
-        'extended/CookieSessionHandler.php' => '/vendor/laravel/framework/src/Illuminate/Session/CookieSessionHandler.php',
         'extended/DatabaseSessionHandler.php' => '/vendor/laravel/framework/src/Illuminate/Session/DatabaseSessionHandler.php',
         'extended/SessionManager.php' => '/vendor/laravel/framework/src/Illuminate/Session/SessionManager.php',
         'extended/SessionServiceProvider.php' => '/vendor/laravel/framework/src/Illuminate/Session/SessionServiceProvider.php',
