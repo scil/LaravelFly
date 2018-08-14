3afd3ad4b3db3c169c41116aefd74aa622f7c735
scil <scil.zc@gmail.com>
2018-08-15 01:58:47 +0800

    add test

--- tests/Map/Unit/FlyTest.php
+++ tests/Map/Unit/FlyTest.php
@@ -12,19 +12,40 @@ class FlyTest extends BaseTestCase
     function testInitEnv()
     {
         $r = self::process(function () {
-            $r[]= defined('WORKER_COROUTINE_ID ');
+            $r[] = defined('WORKER_COROUTINE_ID ');
 
             $initEnv = new \ReflectionMethod('\LaravelFly\Fly', 'initEnv');
             $initEnv->setAccessible(true);
             $initEnv->invoke(null, []);
 
-            $r[]= defined('WORKER_COROUTINE_ID');
-            $r[]= function_exists('tinker');
+            $r[] = defined('WORKER_COROUTINE_ID');
+            $r[] = WORKER_COROUTINE_ID;
+            $r[] = function_exists('tinker');
 
             return $r;
         });
 
-        self::assertEquals([false, true, true], json_decode($r));
+        self::assertEquals([false, true, 1, true], json_decode($r));
+
+    }
+
+    function testEarly_laravel()
+    {
+        $r = self::process(function () {
+            $r[] = defined('WORKER_COROUTINE_ID ');
+
+            $initEnv = new \ReflectionMethod('\LaravelFly\Fly', 'initEnv');
+            $initEnv->setAccessible(true);
+            $initEnv->invoke(null, ['early_laravel'=>true]);
+
+            $r[] = defined('WORKER_COROUTINE_ID');
+            $r[] = WORKER_COROUTINE_ID;
+            $r[] = function_exists('tinker');
+
+            return $r;
+        });
+
+        self::assertEquals([false, true, -1, true], json_decode($r));
 
     }
 
